64294f4dec9ca42aad94506d377a254ac058711c
SnoUweR <snouwer@gmail.com>
2015-06-29 15:53:58 +0700

    перенес копирайт за условие показа автора темы, чтоб было логичнее

--- devdmbootstrap3-child/footer.php
+++ devdmbootstrap3-child/footer.php
@@ -1,9 +1,9 @@
-    <div class="dmbs-footer">
+    <div class="dmbs-footer text-center">
         <?php
             global $dm_settings;
-            if ($dm_settings['author_credits'] != 0) : ?>
-                <div class="row dmbs-author-credits text-center">
-					<p>&copy; <?php echo ax_first_post_date("Y"); ?> - <?php echo date('Y'); ?> <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>. <?php echo __('All Rights Reserved', 'devdmbootstrap3-child') ?>.</p>
+			?><p>&copy; <?php echo ax_first_post_date("Y"); ?> - <?php echo date('Y'); ?> <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>. <?php echo __('All Rights Reserved', 'devdmbootstrap3-child') ?>.</p>
+            <?php if ($dm_settings['author_credits'] != 0) : ?>
+                <div class="row dmbs-author-credits">
 					<p><?php printf( __( 'Template based on: %1$s by %2$s with many changes by %3$s.', 'devdmbootstrap3-child' ), '<a href="'.esc_url( 'http://devdm.com/DevDmBootstrap3/' ).'" title="'.esc_attr('Twitter Bootstrap 3 WordPress Starter Theme for Developers').'">DevDmBootstrap3</a>', 'Danny M', '<a href="'.esc_url( 'http://me.snouwer.ru/' ).'" title="'.esc_attr('Vladislav Kovalev').'">SnoUweR</a>' ); ?></p>
                 </div>
         <?php endif; ?>
